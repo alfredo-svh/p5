1037d6fd2054cd22ad8f502468edbb76250eadcc
Alfredo S <cruzazulini@hotmail.com>
2021-01-26 20:36:40 -0800

    minesweeper 1.0

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -7,67 +7,103 @@
 
 const numCellsWidth = 16;
 const numCellsHeight = 16;
+const totMines = 40;
+
 
 let board = [];
+let bGameOver = false;
+let bWin = false;
+let tilesToGo = numCellsHeight* numCellsWidth - totMines;
+let flaggedMines = 0;
 
-let mine = '💥';
+let hidden = '🟦';
 let flag = '⛳';
 let question = '❓';
+let mine = '💥';
 
-let curPlayer = 0;
-let moves = 0;
 
-let bGameOver = false;
-let winner = null;
+function Cell(){
+	this.num = 0;
+	this.state = hidden;
+	this.bFlipped = false;
+}
+
 
 function initializeBoard(){
 	for(let i = 0; i < numCellsHeight;i++){
 		board.push([]);
 		for(let j = 0; j < numCellsWidth;j++){
-			board[i].push('');
+			board[i].push(new Cell());
 		}
 	}
 }
 
-function isGameOver(){
-	// horizontal
-	for(let i =0;i<3;i++){
-		if(board[i][0] != '' && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
-			bGameOver = true;
-			winner = players[curPlayer];
-			return bGameOver;
+function setNumbers(yCor, xCor){
+	for(let i = yCor-1; i < yCor + 2; i++){
+		for(let j = xCor-1; j < xCor + 2; j++){
+			if(i > -1 && j > -1 && i < numCellsHeight && j < numCellsWidth && board[i][j].num != -1){
+				board[i][j].num++;
+			}
 		}
 	}
-	// vertical
-	for(let i =0;i<3;i++){
-		if(board[0][i] != '' && board[0][i] == board[1][i] && board[1][i] == board[2][i]){
-			bGameOver = true;
-			winner = players[curPlayer];
-			return bGameOver;
+
+}
+
+function placeMinesAndNumbers(){
+	let yCor = int(random(0, numCellsHeight));
+	let xCor = int(random(0, numCellsWidth));
+
+	for(let i = 0; i <totMines; i++){
+		while(board[yCor][xCor].num == -1){
+			yCor = int(random(0, numCellsHeight));
+			xCor = int(random(0, numCellsWidth));
 		}
-	}
-	// diagonal
-	if(board[0][0] != '' && board[0][0] == board[1][1] && board[1][1] == board[2][2]){
-		bGameOver = true;
-		winner = players[curPlayer];
-		return bGameOver;
-	}
-	if(board[0][2] != '' && board[0][2] == board[1][1] && board[1][1] ==board[2][0]){
-		bGameOver = true;
-		winner = players[curPlayer];
-		return bGameOver;
+
+		board[yCor][xCor].num = -1;
+
+		// set numbers around mine
+		setNumbers(yCor, xCor);
 	}
 
-
-	if(!bGameOver && moves == 9){
-		bGameOver = true;
-	}
-
-	return bGameOver;
+	
 }
 
 
-function mousePressed(){
+function clearCluster(i, j){
+	let curCell = board[i][j];
+
+	if(curCell.bFlipped == true){
+		return;
+	}
+
+	if(curCell.num > 0){
+		curCell.state = " "+ curCell.num.toString();
+		curCell.bFlipped = true;
+		tilesToGo--;
+
+		return;
+	}
+
+	curCell.state = ' ';
+	curCell.bFlipped = true;
+	tilesToGo--;
+
+	if(j < numCellsWidth - 1){
+		clearCluster(i, j + 1);
+	}
+	if(i > 0){
+		clearCluster(i - 1, j);
+	}
+	if(j>0){
+		clearCluster(i, j - 1);
+	}
+	if(i < numCellsHeight - 1){
+		clearCluster(i + 1, j);
+	}
+}
+
+
+function mouseReleased(){
 	if(bGameOver || mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height){
 		return;
 	}
@@ -75,58 +111,99 @@ function mousePressed(){
 	let i = floor(mouseY / height * numCellsHeight);
 	let j = floor(mouseX / width * numCellsWidth);
 
-	if(board[i][j] == ''){
-		board[i][j] = mine;
-		// moves++;
-		// if(!isGameOver()){
-		// 	changeCurPlayer();
-		// }
+	let cellClicked = board[i][j];
+
+	
+	if(cellClicked.bFlipped){
+		return;
+	}
+	
+	if(mouseButton == LEFT){
+
+		if(cellClicked.num == -1){
+			cellClicked.state = mine;
+			cellClicked.bFlipped = true;
+
+			bGameOver = true;
+
+			return;
+		}
+		else if(cellClicked.num == 0){
+			clearCluster(i, j);
+
+		}else{
+			cellClicked.state = " "+ cellClicked.num.toString();
+			cellClicked.bFlipped = true;
+			tilesToGo--;
+		}
+
+		if(tilesToGo <= 0){
+			bGameOver = true;
+			bWin = true;
+		}
+		
+	}
+	else if(mouseButton == RIGHT){
+		if(cellClicked.state == hidden){
+			cellClicked.state = flag;
+			flaggedMines++;
+		}
+		else if(cellClicked.state == flag){
+			cellClicked.state = question;
+			flaggedMines--;
+		}
+		else{
+			cellClicked.state = hidden;
+		}
 	}
 }
 
 
 function setup() {
 	createCanvas(730, 730);
-	background(220);
-
-	let resultStr = '';
-
-	initializeBoard();
-	
-	// draw board
-	y = height / numCellsHeight;
-	x = width / numCellsWidth;
-	strokeWeight(4);
-	for(let i =1; i < numCellsHeight;i++){
-		line(0, y*i, width,y*i);
-	}
-	for(let i =1; i<numCellsWidth;i++){
-		line(x*i, 0, x*i, height);
-	}
-}
-
-function draw() {
-	// put drawing code here
 	textSize(height/numCellsHeight - 15);
 	textAlign(LEFT, TOP);
 
-	// drawing moves
+	x = width / numCellsWidth;
+	y = height / numCellsHeight;
+
+	initializeBoard();
+	placeMinesAndNumbers();
+	
+	// draw board
+	// strokeWeight(4);
+	// for(let i =1; i < numCellsHeight;i++){
+	// 	line(0, y*i, width,y*i);
+	// }
+	// for(let i =1; i<numCellsWidth;i++){
+	// 	line(x*i, 0, x*i, height);
+	// }
+
+	score = createP("Mines left: " + (totMines - flaggedMines).toString()).style('color', '#000').style('font-size', '32pt');
+	
+}
+
+function draw() {
+	// drawing the board
+	clear();
+	background(220);
 	for(let i = 0; i < numCellsHeight;i++){
 		for(let j = 0; j < numCellsWidth;j++){
 			// text(board[i][j], j * x, i * y, (j + 1) * x, (i+1) * y);
-			text(board[i][j], j * x+2, i * y+10);
+			text(board[i][j].state, j * x+1, i * y+9);
 		}
 	}
+	score.html("Mines left: " + (totMines - flaggedMines).toString());
 
 	if(bGameOver){
 		noLoop();
-		if(winner == null){
-			resultStr = "It's a tie!";
+		if(bWin){
+			resultStr = "Congrats! You won!";
 		}
 		else{
-			resultStr = winner + " is the winner!"
+			resultStr = "You lost! Try again."
 		}
 
-		createP(resultStr).style('color', '#000').style('font-size', '32pt');
+		score.html(resultStr);
 	}
 }
