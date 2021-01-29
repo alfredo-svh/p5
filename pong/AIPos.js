574d59747e61c0e234613f38f5285750c54ca0b2
Alfredo S <cruzazulini@hotmail.com>
2021-01-29 00:58:22 -0800

    pong 2.0

--- pong/AIPos.js
+++ pong/AIPos.js
@@ -1,31 +1,26 @@
-// TODO make it better
-// ideas:
-// - lerp?
-// invisible ball
+/*
+	File: AIPos.js
 
+	Contains the function the AI uses to update its Y position for the game of Pong
+	
+*/
 
 function getAIPos(curY, ball){
+	// modify this value to change difficulty
+	let speed = 4;
+
+
 	// t = Dx / Vx
 	let t = abs(ball.x - 30) / abs(ball.speedX);
 	
 	// Dy = Vy * t
 	let dY = ball.y + (ball.speedY * t);
 
-
-	// perfect ai
-	// if(dY > HEIGHT){
-	// 	dY = 2*HEIGHT - dY;
-	// }
-	// else if(dY < 0){
-	// 	dY =  - dY;
-	// }
-
-	// imperfect ai
 	if( dY > curY + padHalfLen){
-		return curY + 4;
+		return curY + speed;
 	}
 	else if(dY < curY - padHalfLen){
-		return curY - 4;
+		return curY - speed;
 	}
 	
 	return curY;
--- pong/pong.js
+++ pong/pong.js
@@ -6,27 +6,30 @@
 */
 
 
-// TODO:
-// - angular bounce
 
 const HEIGHT = 600;
 const WIDTH = 900;
 const SPEEDINCREASE = 1.01;
 
+
 let humanScore = 0;
 let AIScore = 0;
-
 
 let bStart = true;
 let bGameOver = false;
 let bWin = false;
+let toRight = true;
+let fromUp = true;
 
 let padLen;
 let padHalfLen;
 
-let toRight = true;
+let p;
+let btn;
 
 
+// ball class which contains its location and speed
+// it also contains the function to update its position and speed based on collisions
 class Ball{
 	constructor() {
 		this.x = 0;
@@ -39,18 +42,31 @@ class Ball{
 		let nextX = this.x + this.speedX;
 		let nextY = this.y + this.speedY;
 
+		let distAI = nextY - AIY;
+		let distHuman = humanY - nextY;
+
 		// AI pad collision
-		if(nextX <= 30 && abs(AIY - nextY) <= padHalfLen && this.x > 30){
+		if(nextX <= 30 && abs(distAI) <= padHalfLen + 10 && this.x > 30){
+			// flip the direction of its speed
 			nextX = 60 - nextX;
 			this.speedX = - (this.speedX * SPEEDINCREASE);
+
+			// calculate new y direction based on the part of the paddle hit
+			this.speedY =  tan(distAI*45 / padHalfLen) * this.speedX;
+			nextY = this.y + this.speedY;
 		}
 		// human pad collision
-		else if(nextX >= WIDTH - 30 && abs(humanY - nextY) <= padHalfLen && this.x < WIDTH - 30){
+		else if(nextX >= WIDTH - 30 && abs(distHuman ) <= padHalfLen + 10 && this.x < WIDTH - 30){
+			// flip the direction of its speed
 			nextX = 2*WIDTH- 60 - nextX;
 			this.speedX = - (this.speedX * SPEEDINCREASE);
+
+			// calculate new y direction based on the part of the paddle hit
+			this.speedY = tan(distHuman*45 / padHalfLen) * this.speedX;
+			nextY = this.y + this.speedY;
 		}
 		// top wall collision
-		if(nextY <= 10){
+		else if(nextY <= 10){
 			nextY = 20 - nextY;
 			this.speedY = - this.speedY;
 		}
@@ -63,14 +79,10 @@ class Ball{
 		this.x = nextX;
 		this.y = nextY;
 	}
-
-	updateSpeed(x, y){
-		this.speedX = x;
-		this.speedY = y;
-	}
 }
 
 
+// pad class. It contains the y position and its min and max possible values of the paddles
 class Pad{
 	constructor(pos, minY, maxY) {
 		this.posY = pos;
@@ -92,10 +104,29 @@ class Pad{
 }
 
 
+function restart(){
+	humanScore = 0;
+	AIScore = 0;
+
+	bStart = true;
+	bGameOver = false;
+	bWin = false;
+	toRight = true;
+	fromUp = true;
+
+	p.remove();
+	btn.remove();
+
+	loop();
+}
+
+
 function setup() {
 	createCanvas(WIDTH, HEIGHT);
 	textSize(50);
 	fill(255);
+	stroke(255);
+	angleMode(DEGREES);
 
 	padLen = HEIGHT / 5;
 	padHalfLen = HEIGHT / 10;
@@ -110,36 +141,43 @@ function draw() {
 	// restart ball from middle
 	if(bStart){
 		ball.x = WIDTH / 2;
-		ball.y = HEIGHT / 2;
+
+		ball.y = fromUp? HEIGHT / 4 : HEIGHT* 3 / 4;
 		
 		ball.speedX = toRight? 7 : -7;
 		ball.speedY = random(-10, 10);
 		
 		bStart = false;
-		toRight = !toRight;
 	}
 
 	clear();
 	background(0);
 
-	// pads
+	// drawing the net
+	for(let i = 0; i < HEIGHT; i+=40){
+		line(WIDTH/2, i, WIDTH/2, i+20);
+	}
+
+	// updating the position of the paddles
 	AIPad.updatePos(getAIPos(AIPad.posY, ball));
 	humanPad.updatePos(mouseY);
 
+	// drawing the paddles
 	rect(10, AIPad.posY - padHalfLen, 10, padLen);
 	rect(WIDTH - 20, humanPad.posY - padHalfLen, 10, padLen);
 	
-	// ball
+	// drawing the ball
 	ball.updatePos(AIPad.posY, humanPad.posY);
 	circle(ball.x, ball.y, 20);
 	
-
 	//check for out of bounds / game over
 	if(ball.x <= -10){
 		bStart = true;
 		humanScore++;
+		toRight = false;
+		fromUp = !fromUp;
 
-		if(humanScore == 6){
+		if(humanScore == 11){
 			bGameOver = true;
 			bWin = true;
 		}
@@ -147,14 +185,16 @@ function draw() {
 	else if(ball.x >= WIDTH + 10){
 		bStart = true;
 		AIScore++;
+		toRight = true;
+		fromUp = !fromUp;
 
-		if(AIScore == 6){
+		if(AIScore == 11){
 			bGameOver = true;
 		}
 	}
-	
 
-	//score
+	
+	// drawing the score
 	text(AIScore.toString(), WIDTH/2 - 70, 50);
 	text(humanScore.toString(),WIDTH/2 + 50, 50);
 
@@ -168,6 +208,8 @@ function draw() {
 			resultStr = "You lost! Try again."
 		}
 
-		createP(resultStr).style('color', '#000').style('font-size', '32pt');
+		p = createP(resultStr).style('color', '#000').style('font-size', '32pt');
+		btn = createButton('Play again?');
+		btn.mousePressed(restart);
 	}
 }
